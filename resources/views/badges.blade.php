1d6ed0ae9215be0d62a7b05549d2ffa7e873248b
SarahSibert <mail@sarahsibert.com>
2021-11-05 15:51:51 +0000

    Update badges

--- resources/views/badges.blade.php
+++ resources/views/badges.blade.php
@@ -8,12 +8,12 @@
                 <hr class="mb-8" />
 
                 <div class="mb-8 space-y-4">
-                    <x-badge color="bright-orange">Badge</x-badge>
-                    <x-badge color="logo-orange">Badge</x-badge>
-                    <x-badge color="maroon">Badge</x-badge>
-                    <x-badge color="dark-gray">Badge</x-badge>
-                    <x-badge color="pale-brown">Badge</x-badge>
-                    <x-badge color="beige">Badge</x-badge>
+                    <x-badge color="bright-orange">Bright Orange Badge</x-badge>
+                    <x-badge color="logo-orange">Logo Orange Badge</x-badge>
+                    <x-badge color="maroon">Maroon Badge</x-badge>
+                    <x-badge color="dark-gray">Dark Gray Badge</x-badge>
+                    <x-badge color="pale-brown">Pale Brown Badge</x-badge>
+                    <x-badge color="beige">Beige Badge</x-badge>
                 </div>
             </div>
         </div>
--- resources/views/components/badge.blade.php
+++ resources/views/components/badge.blade.php
@@ -25,5 +25,5 @@
     {{ $attributes->merge(['class' => 'inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium '.$colors]) }}
     {{ $attributes->whereStartsWith('x-') }}
     {{ $attributes->thatStartWith('wire:') }}>
-    Badge
+    {{ $slot }}
 </span>
