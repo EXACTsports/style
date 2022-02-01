4cb0eac313efed0e84a91a5c20ab3a1f49dc28a2
Sarah Sibert <6719178-SarahNS74@users.noreply.gitlab.com>
2022-02-01 15:35:09 +0000

    Update tooltips

--- resources/views/components/tooltip.blade.php
+++ resources/views/components/tooltip.blade.php
@@ -1,15 +1,15 @@
-@props(['title' => '', 'abbr' => ''])
+@props(['title' => '', 'abbr' => '', 'color' => 'light'])
 
 <div 
     x-data="{ tooltip: false }" 
     x-on:mouseover="tooltip = true" 
     x-on:mouseleave="tooltip = false"
-    class="cursor-pointer">
+    class="cursor-pointer ">
       {{$abbr}}
       <div 
         x-show="tooltip"
-        class="absolute p-2 text-sm text-white transform translate-x-0 translate-y-0 bg-gray-800 rounded-lg">
-        {{$title}}
+        class="absolute p-2 text-sm transform translate-x-0 translate-y-0 border rounded-lg  {{ $color === 'dark' ? 'bg-gray-800 border-gray-900 text-gray-100' : 'text-gray-800 bg-gray-50 border-gray-600'}}">
+        <span class="font-medium">{{$title}}</span>
         <br />
         {{$slot}}
       </div>
--- resources/views/tooltips.blade.php
+++ resources/views/tooltips.blade.php
@@ -7,7 +7,7 @@
         <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
             <div class="flex p-6 space-x-4">
                 <x-tooltip abbr="TT" title="TOOLTIP 1">This is the Tooltip Text</x-tooltip>
-                <x-tooltip abbr="TT" title="TOOLTIP 2">This is the Tooltip Text</x-tooltip>
+                <x-tooltip abbr="TT" title="TOOLTIP 2" color="dark">This is the Tooltip Text</x-tooltip>
             </div>
         </div>
     </div>
